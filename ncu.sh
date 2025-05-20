a06721732d2e76a550a3ee135628d35df99903a0
Antony Holmes <antony@antonyholmes.dev>
2025-05-20 09:51:00 -0400

    fix: Bug fixes and updates.

---
+++ ncu.sh
@@ -0,0 +1,8 @@
+ncu -u
+pnpm install
+#pnpm install tailwindcss@3
+#pnpm install react@18
+#pnpm install react-dom@18
+#pnpm install @types/react-dom@18
+#pnpm install @types/react@18
+#pnpm remove @astrojs/tailwind
